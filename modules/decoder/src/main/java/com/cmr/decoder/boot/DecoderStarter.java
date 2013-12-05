0ca9532f478d7fb37073abf620a992cfd4206149
rajithd <rajithgama@gmail.com>
2013-12-05 16:27:59 +0530

    added decoder module

---
+++ modules/decoder/src/main/java/com/cmr/decoder/boot/DecoderStarter.java
@@ -0,0 +1,17 @@
+package com.cmr.decoder.boot;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author rajith
+ */
+public class DecoderStarter {
+
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(DecoderStarter.class);
+
+    public static void main(String[] args) {
+        logger.info("################################################");
+        logger.info("##  Decoder Started Successfully  ##");
+        logger.info("################################################");
+    }
+}
