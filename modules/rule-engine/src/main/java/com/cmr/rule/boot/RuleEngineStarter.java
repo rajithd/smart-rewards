5013fe232f45a5afaf1545c81a06d1883aeac327
rajithd <rajithgama@gmail.com>
2014-02-22 13:52:19 +0530

    initial commit for ruleengine

--- modules/rule-engine/src/main/java/com/cmr/rule/boot/RuleEngineStarter.java
+++ modules/rule-engine/src/main/java/com/cmr/rule/boot/RuleEngineStarter.java
@@ -1,9 +1,19 @@
 package com.cmr.rule.boot;
 
 
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class RuleEngineStarter {
 
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(RuleEngineStarter.class);
+
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("rule-context.xml");
+        logger.info("####################################################");
+        logger.info("#####       Rule Engine Started Successfully   #####");
+        logger.info("####################################################");
 
     }
 }
